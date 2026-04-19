5f5c60fdbc513444d34dbe07df2ac4f056d27315
kunalkatiyarr <kunalkatiyar916@gmail.com>
2026-04-19 10:05:13 +0530

    feat: implement User entity with security credentials and profile attributes

---
+++ src/main/java/com/wealthmetrics/entity/User.java
@@ -0,0 +1,26 @@
+package com.wealthmetrics.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.ROLE_USER;
+}
